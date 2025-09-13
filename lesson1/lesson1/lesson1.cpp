84b4f0a9a7ffcb0ce738e14d1c1ea5de12b7ccba
MyhailoDudar <miha.du2010@gmail.com>
2025-09-13 09:23:06 +0300

    nuw function

--- lesson1/lesson1/lesson1.cpp
+++ lesson1/lesson1/lesson1.cpp
@@ -1,6 +1,6 @@
 
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
 void eval(int num) {
@@ -13,10 +13,30 @@ void eval(int num) {
 		cout << "the num is odd" << endl;
 	}
 }
+int findLargest(vector<int> nums) {
+	int lrg=nums[0];
+	for (int i = 1; i <= nums.size()-1; i++)
+	{
+		if (nums[i]>lrg)
+		{
+			lrg = nums[i];
+		}
+	}
+	return lrg;
+}
 
 int main()
 {
+	vector<int> nums = {
+		5, 12, 23, 7, 45,
+		18, 29, 33, 41, 56,
+		67, 72, 84, 91, 100,
+		3, 9, 14, 27, 39, -371
+	};
 	int num;
+	num = findLargest(nums);
+	cout << num << endl;
+	
 	while (true)
 	{
 		cin >> num;
