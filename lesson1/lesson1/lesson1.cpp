bd8fa158914f378788a0289a8e9f7c6bbbd2656a
MyhailoDudar <miha.du2010@gmail.com>
2025-09-13 09:00:59 +0300

    the first test task

---
+++ lesson1/lesson1/lesson1.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int num;
+	while (true)
+	{
+		cin >> num;
+		if (num%2==0)
+		{
+			cout << "the num is even" << endl;
+		}
+		else
+		{
+			cout << "the num is odd" << endl;
+		}
+	}
+}
+
