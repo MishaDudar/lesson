908a05c62c4f0537fe02ee9c79f0e1275fbb658d
MyhailoDudar <miha.du2010@gmail.com>
2025-09-13 09:10:22 +0300

    function fix

--- lesson1/lesson1/lesson1.cpp
+++ lesson1/lesson1/lesson1.cpp
@@ -3,20 +3,24 @@
 
 using namespace std;
 
+void eval(int num) {
+	if (num % 2 == 0)
+	{
+		cout << "the num is even" << endl;
+	}
+	else
+	{
+		cout << "the num is odd" << endl;
+	}
+}
+
 int main()
 {
 	int num;
 	while (true)
 	{
 		cin >> num;
-		if (num%2==0)
-		{
-			cout << "the num is even" << endl;
-		}
-		else
-		{
-			cout << "the num is odd" << endl;
-		}
+		eval(num);
 	}
 }
 
